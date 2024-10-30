8cad295d88ce3d2510adc4f60f0feaae4dce997b
SaifeeLucky <sK.saifee@webskitters.in>
2024-10-30 16:25:59 +0530

    s

--- code_point/urls.py
+++ code_point/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', homeRender, name='home')
+    path('', homeRender, name='home')
     # path('', include('code_point.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
